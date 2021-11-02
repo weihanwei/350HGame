8eccebdfa4b470e236e8fcf788fca9b87c5cc9e2
weihanwei <407427270@qq.com>
2021-11-02 16:02:02 -0400

    commit !
    commit !

---
+++ 350h/Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public playermovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+       // Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Obstcle") {
+        //    Debug.Log(" we hit Obstcle");
+            movement.enabled = false;
+        }
+    }
+}
---
+++ 350h/Assets/playermovement.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public Rigidbody rd;
+    public float forwardForce = 2000f;
+    public float sodewaysForace = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello ,world!");
+        rd.useGravity = false;
+       // rd.addForce();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input.GetKey("w");
+    }
+    void FixedUpdate() {
+        rd.AddForce(0,0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d")) {
+            rd.AddForce(sodewaysForace * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rd.AddForce(-sodewaysForace * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
